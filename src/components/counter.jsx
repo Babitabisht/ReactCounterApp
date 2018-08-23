d537b17990a53c78db1eb3c9e9f771184bcf7cc6
Babita Bisht <babitabisht2018@gmail.com>
2018-08-23 21:31:51 +0530

    second commit

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,10 +7,10 @@ class Counter extends Component {
   //      this.handleIncrement=this.handleIncrement.bind(this);
   //  }
 
-  state = {
-    value: this.props.value,
-    tags: []
-  };
+  // state = {
+  //   value: this.props.counter.value,
+  //   tags: []
+  // };
 
   renderTags() {
     if (this.state.tags.length === 0) return <p>There are no tags </p>;
@@ -24,12 +24,13 @@ class Counter extends Component {
     );
   }
 
-  handleIncrement = () => {
-    console.log();
-    this.setState({
-      value: this.state.value + 1
-    });
-  };
+  // handleIncrement = () => {
+  //   console.log();
+  //   this.setState({
+  //     value: this.state.value + 1
+  //   });
+  // };
+
 
   // styles={
   //     fontSize:50,
@@ -40,15 +41,21 @@ class Counter extends Component {
     return (
       <div>
         {this.props.children}
-        <span className="btn-primary btn-sm m-2"> {this.state.value} </span>
+        <span className="btn-primary btn-sm m-2"> {this.props.counter.value} </span>
         <button
-          onClick={() => this.handleIncrement()}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-       <button className="btn btn-danger btn-sm m-2">
-       Delete</button>
+
+        <button
+        
+          className="btn btn-danger btn-sm m-2" 
+          onClick={()=> this.props.onDelete(this.props.counter.id)}
+        >
+          Delete
+        </button>
         <br />
         <hr />
       </div>
